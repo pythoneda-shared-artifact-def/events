d68789adcad71f703a0fdd6daf8ccae6f411e661
rydnr <github@acm-sl.org>
2023-07-26 21:19:23 +0200

    Updated pythoneda-shared-pythoneda-domain to 0.0.1a26 Updated pythoneda-shared-artifact-changes-shared to 0.0.1a2

--- events/flake.nix
+++ events/flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-shared-artifact-changes-shared = {
       url =
-        "github:pythoneda-shared-artifact-changes/shared-artifact/0.0.1a1?dir=shared";
+        "github:pythoneda-shared-artifact-changes/shared-artifact/0.0.1a2?dir=shared";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-domain.follows =
@@ -13,7 +13,7 @@
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a25?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a26?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
