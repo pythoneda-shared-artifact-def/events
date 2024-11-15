59cebb0c52ca236cf57160f54d252a1d333ae04c
rydnr <github@acm-sl.org>
2024-11-15 14:17:09 +0100

    Support for Python 3.12
    Moved pyproject.toml.template to templates/

--- flake.nix
+++ flake.nix
@@ -72,8 +72,8 @@
           in python.pkgs.buildPythonPackage rec {
             inherit pname version;
             projectDir = ./.;
-            pyprojectTemplateFile = ./pyprojecttoml.template;
-            pyprojectTemplate = pkgs.substituteAll {
+            pyprojectTomlTemplate = ./templates/pyproject.toml.template;
+            pyprojectToml = pkgs.substituteAll {
               authors = builtins.concatStringsSep ","
                 (map (item: ''"${item}"'') maintainers);
               desc = description;
@@ -82,7 +82,7 @@
               pythonedaSharedPythonlangDomainVersion =
                 pythoneda-shared-pythonlang-domain.version;
               unidiffVersion = python.pkgs.unidiff.version;
-              src = pyprojectTemplateFile;
+              src = pyprojectTomlTemplate;
             };
             src = pkgs.fetchFromGitHub {
               owner = org;
@@ -104,8 +104,8 @@
               cp -r ${src} .
               sourceRoot=$(ls | grep -v env-vars)
               chmod -R +w $sourceRoot
-              cat ${pyprojectTemplate}
-              cp ${pyprojectTemplate} $sourceRoot/pyproject.toml
+              cat ${pyprojectToml}
+              cp ${pyprojectToml} $sourceRoot/pyproject.toml
             '';
 
             postInstall = ''
@@ -129,7 +129,7 @@
         devShells = rec {
           default = pythoneda-shared-artifact-events-default;
           pythoneda-shared-artifact-events-default =
-            pythoneda-shared-artifact-events-python311;
+            pythoneda-shared-artifact-events-python312;
           pythoneda-shared-artifact-events-python38 = shared.devShell-for {
             banner = "${
                 pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python38
@@ -186,11 +186,25 @@
               pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
             inherit archRole layer org pkgs repo space;
           };
+          pythoneda-shared-artifact-events-python312 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.pythoneda-shared-artifact-events-python312;
+            python = pkgs.python312;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python312;
+            inherit archRole layer org pkgs repo space;
+          };
         };
         packages = rec {
           default = pythoneda-shared-artifact-events-default;
           pythoneda-shared-artifact-events-default =
-            pythoneda-shared-artifact-events-python311;
+            pythoneda-shared-artifact-events-python312;
           pythoneda-shared-artifact-events-python38 =
             pythoneda-shared-artifact-events-for {
               python = pkgs.python38;
@@ -214,6 +228,12 @@
               python = pkgs.python311;
               pythoneda-shared-pythonlang-domain =
                 pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
+            };
+          pythoneda-shared-artifact-events-python312 =
+            pythoneda-shared-artifact-events-for {
+              python = pkgs.python312;
+              pythoneda-shared-pythonlang-domain =
+                pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python312;
             };
         };
       });
